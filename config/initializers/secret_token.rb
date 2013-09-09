c2e98abbddfbfec2b56e75841601967f0e8026c9
moizest89 <moises.gilberto@gmail.com>
2013-09-09 13:26:14 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MoizestWebsite::Application.config.secret_token = '1c2f6e86ed0b154e41afb2a45389e6d9fdac69c7698899255ed60b6222a495f4fa9eec297758fae85deb0b4b579b9038a21c5999c60e42a08943a9b540197172'
